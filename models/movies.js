82963b4fc4f5a4484764cc5fd6b0808798563539
STEVE NEPOTE <senepote@gmail.com>
2018-07-18 16:22:04 -0500

    schema created and table started

---
+++ models/movies.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const movieSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  likes: {type: Number, default: 0}
+})
+
+module.exports = mongoose.model('Movies', movieSchema)
